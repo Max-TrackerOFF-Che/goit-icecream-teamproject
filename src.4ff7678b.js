aa3f5eaaaf6ec3930a98345d65d7d963761db5d7
Max-TrackerOFF-Che <makassi12@gmail.com>
2022-01-28 17:45:14 +0000

    Deploying to gh-pages from @ Max-TrackerOFF-Che/goit-icecream-teamproject@10f35fc6025a1631a6fbcb2824f3deeaac8da53c 🚀

--- src.4ff7678b.js
+++ src.4ff7678b.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-icecream-teamproject/src.f35e9ca7.js.map
+//# sourceMappingURL=/goit-icecream-teamproject/src.4ff7678b.js.map
