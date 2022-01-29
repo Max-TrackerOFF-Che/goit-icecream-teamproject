50594ee70618163778c8d315869e5d4ee7acde85
Liubov Orlova <lyubao7749@gmail.com>
2022-01-29 17:55:41 +0200

    #2-осталось доработать стили

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
   $('.single-item').slick({
     dots: true,
     arrows: false,
+    // slidesToShow: 1,
+    //  slidesToScroll: 1,
+    //  autoplay: true,
+    //  autoplaySpeed: 2000,
     dotsClass: 'dots-style',
   });
 });
