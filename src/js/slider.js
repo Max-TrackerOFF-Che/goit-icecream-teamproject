67bbddd6326da63a2bead31d15578be0d194f5be
Liubov Orlova <lyubao7749@gmail.com>
2022-01-30 00:00:18 +0200

    bug fixed style

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
   $('.single-item').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     dots: true,
     arrows: false,
-    // slidesToShow: 1,
-    //  slidesToScroll: 1,
-    //  autoplay: true,
-    //  autoplaySpeed: 2000,
     dotsClass: 'dots-style',
   });
 });
