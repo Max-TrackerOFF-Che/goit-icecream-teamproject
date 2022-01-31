d1c6a53398668528b77e4439bb06e18e9fff7c0c
khodetsky <hodyaya@gmail.com>
2022-01-31 21:02:02 +0200

    Update mob-menu.js
    change script for mob-menu

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,13 +1,15 @@
 (() => {
   const menuBtnRef = document.querySelector('.my-class1');
-  const mobileMenuRef = document.querySelector('.my-class2');
+  const mobileMenuRef = document.querySelectorAll('.my-class2');
   const menuContainerRef = document.querySelector('.header__mob-menu');
   console.log(menuBtnRef);
-  mobileMenuRef.addEventListener('click', () => {
+  mobileMenuRef.forEach(function (item) {
+    item.addEventListener('click', () => {
     menuContainerRef.classList.toggle('is-open');
+    });
   });
   menuBtnRef.addEventListener('click', () => {
     console.log(222);
     menuContainerRef.classList.toggle('is-open');
-  });
+    });
 })();
